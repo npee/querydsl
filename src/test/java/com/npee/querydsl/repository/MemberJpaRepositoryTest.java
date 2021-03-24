785d3e225b080de7cf5b65ddd6cce954e523d8e1
npee <jmc4664@gmail.com>
2021-03-24 20:14:21 +0900

    순수 JPA Repository 테스트 코드

--- src/test/java/com/npee/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/npee/querydsl/repository/MemberJpaRepositoryTest.java
@@ -1,7 +1,41 @@
 package com.npee.querydsl.repository;
 
+import com.npee.querydsl.domain.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class MemberJpaRepositoryTest {
 
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void basicTest() throws Exception {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all).containsExactly(member);
+
+        List<Member> member1 = memberJpaRepository.findByUsername("member1");
+        assertThat(member1).containsExactly(member);
+    }
 }
