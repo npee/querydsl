03e1437f158c64b7f409c9dcd95b026f201f7eb7
npee <jmc4664@gmail.com>
2021-03-25 19:25:47 +0900

    JPA Repository의 Querydsl버전 코드 작성

--- src/main/java/com/npee/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/com/npee/querydsl/repository/MemberJpaRepository.java
@@ -1,12 +1,15 @@
 package com.npee.querydsl.repository;
 
 import com.npee.querydsl.domain.entity.Member;
+import com.npee.querydsl.domain.entity.QMember;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
+
+import static com.npee.querydsl.domain.entity.QMember.*;
 
 @Repository
 public class MemberJpaRepository {
@@ -33,9 +36,20 @@ public class MemberJpaRepository {
                 .getResultList();
     }
 
+    public List<Member> findAll_Querydsl() {
+        return queryFactory.selectFrom(member)
+                .fetch();
+    }
+
     public List<Member> findByUsername(String username) {
         return em.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", username)
                 .getResultList();
+    }
+
+    public List<Member> findByUsername_Querydsl(String username) {
+        return queryFactory.selectFrom(member)
+                .where(member.username.eq(username))
+                .fetch();
     }
 }
