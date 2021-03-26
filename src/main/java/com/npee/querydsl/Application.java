4b03474364b47dd54694d42e86b06019110547e3
npee <jmc4664@gmail.com>
2021-03-26 20:12:33 +0900

    EntityManager, JPAQueryFactory를 Bean으로 등록하여 사용하기

--- src/main/java/com/npee/querydsl/Application.java
+++ src/main/java/com/npee/querydsl/Application.java
@@ -1,11 +1,20 @@
 package com.npee.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).run();
+    }
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
     }
 }
