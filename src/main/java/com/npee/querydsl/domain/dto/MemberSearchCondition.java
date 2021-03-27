ba9bd2358055ebb35f185ad78238dba4a9a5148d
npee <jmc4664@gmail.com>
2021-03-27 16:50:02 +0900

    Member 검색에 사용할 dto 생성

---
+++ src/main/java/com/npee/querydsl/domain/dto/MemberSearchCondition.java
@@ -0,0 +1,11 @@
+package com.npee.querydsl.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberSearchCondition {
+    private String username;
+    private String teamName;
+    private Integer ageGoe;
+    private Integer ageLoe;
+}
