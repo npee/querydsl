c143a2859fb853ccea76d8f686886f7e426e2c07
npee <jmc4664@gmail.com>
2021-03-27 20:01:10 +0900

    검색 조건으로 조회하는 기능의 컨트롤러 작성

---
+++ src/main/java/com/npee/querydsl/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.npee.querydsl.controller;
+
+import com.npee.querydsl.domain.dto.MemberSearchCondition;
+import com.npee.querydsl.domain.dto.MemberTeamDto;
+import com.npee.querydsl.repository.MemberJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
